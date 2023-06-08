abfbf9878d10b89d7db29f36172230b0eaf5cf9f
Diêgo R. Madureira <71299678+diegomadureira2007@users.noreply.github.com>
2023-06-08 18:10:56 -0300

    Update 02_Meses_Ano-2Modelo.py

--- Desafio_Codigo/01_Desafios_Básicos/02_Meses_do_Ano/02_Meses_Ano-2Modelo.py
+++ Desafio_Codigo/01_Desafios_Básicos/02_Meses_do_Ano/02_Meses_Ano-2Modelo.py
@@ -1,8 +1,3 @@
-#Leia um valor inteiro entre 1 e 12, inclusive.
-#Correspondente a este valor, deve ser apresentado como resposta o mês do ano por extenso, 
-#em inglês, com a primeira letra maiúscula.
-
-
 month = int(input())
 
 months_dict = {
