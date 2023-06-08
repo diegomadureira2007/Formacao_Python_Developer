d10a97cad6bc5c349b91b0aff38d9da80f4fc5ff
Diêgo R. Madureira <71299678+diegomadureira2007@users.noreply.github.com>
2023-06-08 18:04:59 -0300

    Add files via upload

---
+++ Desafio_Codigo/01_Desafios_Básicos/02_Meses_do_Ano/02_Meses_Ano-1Modelo.py
@@ -0,0 +1,37 @@
+n = int(input('Digite um número inteiro: '))
+if 1<=n<=12:
+    if n==1:
+        print("January")
+    elif n==2:
+        print("February")
+    elif n==3:
+        print("March")
+    elif n==4:
+        print("April")
+    elif n==5:
+        print("May")
+    elif n==6:
+        print("June")
+    elif n==7:
+        print("July")
+    elif n==8:
+        print("August")
+    elif n==9:
+        print("September")
+    elif n==10:
+        print("October")
+    elif n==11:
+        print("November")
+    elif n==12:
+        print("December")
+
+
+
+        month = input()
+
+months_dict = {
+    ''' 
+    TODO Faça uma relação entre as possíveis entradas e os meses (em inglês).
+    TODO Imprima o valor de cada chave em relação as entradas do programa.
+    '''
+}
